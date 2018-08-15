ee1442c5999ef77b25f71d7e4626afd568974b65
Jason Huey <jason.huey1@gmail.com>
2018-08-15 10:30:21 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// Using the search function, search using the  searchterm from the FDA database	
 function search(){
 	$("#effects").html('');
 	var key = "7PcXD9iQU905xvCopWU8Nqkiy8GoiEnwpCQkcl5O"
@@ -8,10 +10,12 @@ function search(){
 	   	url: "https://api.fda.gov/drug/event.json?api_key="+ key + "&search=" + searchTerm+"&count=patient.reaction.reactionmeddrapt.exact",
 	   	dataType: "json",
 	   	success: function(data) {
+	   		// shows 15 results before cutting loop
 	   		for(i=0;i<15;i++){
 	    		var results = (data.results[i].term)
           if(data.results[i].term === "DRUG INEFFECTIVE"){
             results[i].term = ""
+            // blanks show up to try another search. 
           }else if(data.results[i].term === ""){
             document.write("Try another search") 
           }else{
